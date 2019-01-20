d0506ceb21c3c2356786be34d39958eaee6264ef
Allan Pichardo <allan.pichardo@gmail.com>
2019-01-19 19:58:17 -0500

    Added lowpass filter to accelerometer logger

---
+++ Assets/Scripts/AccelerometerLogger.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using Filters;
+using UnityEngine;
+
+public class AccelerometerLogger : MonoBehaviour
+{
+    public float smoothing = 0.3f;
+    private LowpassFilter lowpassFilter;
+
+    private void Start()
+    {
+        lowpassFilter = new LowpassFilter();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 filteredAcceleration = lowpassFilter.GetFilteredVector(Input.acceleration, smoothing);
+
+        Debug.Log(string.Format("Accel: X:{0} Y:{1} Z:{2}", filteredAcceleration.x, filteredAcceleration.y, filteredAcceleration.z));
+    }
+
+}
---
+++ Assets/Scripts/LowpassFilter.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Filters
+{
+    public class LowpassFilter
+    {
+        private Vector3 lastVector;
+
+        public LowpassFilter()
+        {
+            this.lastVector = new Vector3();
+        }
+
+        public void SetPreviousValue(Vector3 lastVector)
+        {
+            this.lastVector = lastVector;
+        }
+
+        private float Filter(float unfiltered, float beta, float lastValue)
+        {
+            float Y;
+            Y = beta * unfiltered + (1 - beta) * lastValue;
+            return Y;
+        }
+
+        public Vector3 GetFilteredVector(Vector3 unfiltered, float beta)
+        {
+            float x = Filter(unfiltered.x, beta, lastVector.x);
+            float y = Filter(unfiltered.y, beta, lastVector.y);
+            float z = Filter(unfiltered.z, beta, lastVector.z);
+
+            Vector3 filtered = new Vector3(x, y, z);
+
+            this.lastVector = filtered;
+
+            return lastVector;
+        }
+
+    }
+}
